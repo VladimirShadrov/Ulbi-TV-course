4da4589a7b5dcb57f67af4e38b012c0fb19f8d81
Vladimir Shadrov <shadrov_vladimir@mail.ru>
2024-11-27 11:03:51 +0300

    Декомпозиця конфига

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,33 +1,21 @@
 import path from 'path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
+import { BuildPath } from './src/config/build/types/config';
+import { buildWebpackConfig } from './src/config/build/buildWebpackConfig';
 
-const config: webpack.Configuration = {
-  mode: 'development',
+const paths: BuildPath = {
+  build: path.resolve(__dirname, 'build'),
   entry: path.resolve(__dirname, 'src', 'index.ts'),
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'build'),
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, 'public', 'index.html'),
-    }),
-    new webpack.ProgressPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
+  html: path.resolve(__dirname, 'public', 'index.html'),
 };
+
+const mode = 'development';
+const isDev = mode === 'development';
+
+const config: webpack.Configuration = buildWebpackConfig({
+  mode: 'development',
+  paths,
+  isDev,
+});
 
 export default config;
